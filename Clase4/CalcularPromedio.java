302931b02338dab3eda90a031466e7ecab71e002
Maximiliano Fischer <imaximilianofischer@gmail.com>
2023-03-08 16:14:19 -0300

    Subida de métodos
    Métodos básicos para la sumatoria y el cálculo de promedio.

---
+++ Clase4/CalcularPromedio.java
@@ -0,0 +1,20 @@
+package Clase4;
+
+public class CalcularPromedio {
+    public static void main(String[] args) throws Exception {
+        int[] numeros = new int[] {2, 24, 56};
+
+        int resultado = sumatoria(numeros);
+        int promedio = resultado / numeros.length;  // Calculando el promedio, reutilizando la función "sumatoria"
+
+        System.out.println("El promedio es: " + promedio);
+    }
+
+    public static int sumatoria(int[] numerosASumar) {      // Un "int" es lo que va a retornar la función
+        int resultado = 0;
+        for (int i = 0; i < numerosASumar.length; i++) {
+            resultado += numerosASumar[i];
+        }
+        return resultado;
+    }
+}
---
+++ Clase4/Sumatoria.java
@@ -0,0 +1,31 @@
+package Clase4;
+
+public class Sumatoria {
+    public static void main(String[] args) throws Exception {
+
+        /*
+
+        int numeros[] = new int[]{2, 24, 56};
+        int resultado = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            resultado += numeros[i];
+        }
+        System.out.println(resultado);
+         */
+
+        int[] numeros = new int[]{2, 24, 56};
+
+        System.out.println("Resultado: " + sumatoria(numeros));     // La "impresión" debe estar fuera de la función
+
+    }
+
+    // Función para sumar
+    public static int sumatoria(int[] numerosASumar) {      // Un "int" es lo que va a retornar la función
+        int resultado = 0;
+        for (int i = 0; i < numerosASumar.length; i++) {
+            resultado += numerosASumar[i];
+        }
+        return resultado;
+    }
+
+}
